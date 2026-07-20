82abddaeb4dd77b5bf131efc7ea55083c20557ef
ericxio10 <ericxio2010@gmail.com>
2026-07-19 22:12:47 -0500

    add front end as well

--- frontend/app.js
+++ frontend/app.js
@@ -1,3 +1,6 @@
+let currentfileid = null;
+
+
 async function uploadhandler() {
 	let input = document.getElementById("photo");
 	
@@ -28,6 +31,11 @@ async function uploadhandler() {
 	if(statusmessage == 'success') {
 		document.getElementById("uploadstatus").innerHTML = "success";
 		showpreview(data.fileid);
+		
+		document.getElementById("ocr").style.display = "block"
+
+		
+		currentfileid = data.fileid;
 
 	}
 	
@@ -45,9 +53,81 @@ function showpreview(fileid) {
 	
 	previewelement.style.display="block";
 	
+	document.getElementById("ocr").style.display = "block";
+	
+		document.getElementById("ocr-result").value = "";
+				document.getElementById("ocr-status").textContent = "";
+						document.getElementById("ocr-confidence").textContent = "";
+
+
+
+	
 	
 
 	
 }
 
-document.getElementById("uploader-button").addEventListener("click", uploadhandler)
+async function ocrhandler() {
+	ocrbutton = document.getElementById("ocr-button");
+	
+	ocrbutton.disabled = true;
+	ocrbutton.textContent = "WORKING..."
+	
+	document.getElementById("ocr-status").textContent = "processing...";
+	
+	
+	try {
+		
+		let responce = await fetch("/api/ocr", {
+            method: "POST",
+             headers: { "Content-Type": "application/json" },
+             body: JSON.stringify({ filename: currentfileid })
+          })
+		
+	let data = await responce.json();
+	
+	console.log(data);
+	
+	showpreview(data.fileid);
+	document.getElementById("ocr-result").value = data.text;
+	document.getElementById("ocr-confidence").textContent = "confidence: " + Math.round(data.confidence * 100) + "%";
+	
+	console.log("lines: " + data.lines);
+	
+				document.getElementById("ocr-status").textContent = "ocr success";
+
+
+			document.getElementById("ocr-button").disabled = false;
+			document.getElementById("ocr-button").textContent = "GET TEXT";
+
+	
+	
+
+	
+
+	
+
+	}
+	
+	catch(error){
+			document.getElementById("ocr-status").textContent = "ERROR: " + error;
+			
+			document.getElementById("ocr-button").disabled = false;
+			document.getElementById("ocr-button").textContent = "GET TEXT";
+			
+
+	}
+
+
+	
+	
+	
+}
+
+
+
+
+
+document.getElementById("uploader-button").addEventListener("click", uploadhandler);
+document.getElementById("ocr-button").addEventListener("click", ocrhandler)
+
