ab64478e6b5b8b338a5030e9dd7630c1d511a1fb
ericxio <ericxio2010@gmail.com>
2026-07-26 22:05:51 -0500

    prototype for card frontend [no css yet]

--- frontend/app.js
+++ frontend/app.js
@@ -90,6 +90,10 @@ async function ocrhandler() {
 	
 	showpreview(data.fileid);
 	document.getElementById("ocr-result").value = data.text;
+	document.getElementById("form-ocrtext").value = data.text;
+	
+	document.getElementById("form-imagepath").value = data.fileid;
+	
 	document.getElementById("ocr-confidence").textContent = "confidence: " + Math.round(data.confidence * 100) + "%";
 	
 	console.log("lines: " + data.lines);
@@ -132,13 +136,136 @@ function tabswitcher(tab) {
     element.hidden = false;
 	});
 
-	document.querySelectorAll(".section:not(#"+tab+")").forEach(element => {
+	document.querySelectorAll(".section:not(#"+tab+"-section)").forEach(element => {
     element.hidden = true;
 	});
 }
 
+function showcardform() {
+	tabswitcher("cardform")
+	
+	//document.getElementById("form-ocrtext").value = document.getElementById("ocr-result").value;
+	
+	document.getElementById("form-profile").value = "";
+	document.getElementById("form-productname").value = "";
+	document.getElementById("form-strength").value = "";
+	document.getElementById("form-instructions").value = "";
+	document.getElementById("form-notes").value = "";
+	document.getElementById("form-time").value = "";
+	
+	document.getElementById("form-status").value = "";
+
+	
+	
+}
+
+function savecard() {
+	let profile = 	document.getElementById("form-profile").value;
+	let productname = document.getElementById("form-productname").value;
+	let strength = document.getElementById("form-strength").value;
+	let instructions  = document.getElementById("form-instructions").value;
+	let notes = document.getElementById("form-notes").value;
+	let time = document.getElementById("form-time").value;
+	
+	if (profile == "" || productname == "") {
+		document.getElementById("form-status").value = "error: profile or product name missing";
+		
+		return;
+	}
+	
+	let send = {
+              profile_name: profile,
+              product_name: productname,
+              strength: strength,        // empty string becomes null
+              directions: instructions,
+			  warnings: "",
+              personal_notes: notes,
+              reminder_times: time,
+              ocr_text: document.getElementById("form-ocrtext").value,
+              image_path: document.getElementById("form-imagepath").value
+          }
+		  
+	document.getElementById("form-status").value = "saving..."
+	
+	fetch("/api/cards/" + send.image_path, {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify(send)
+          })
+		  
+	document.getElementById("form-status").value = "card saved"
+	
+	
+
+
+
+}
+
+function cancelform() {
+	tabswitcher("cards");
+	
+}
+
+async function showcardlist() {
+	tabswitcher("cards");
+	let container = document.getElementById("card-list-container");
+	
+	container.innerHTML = "";
+	
+	let data = await fetch("/api/cards/"
+              
+             
+          )
+		  
+	cards = await data.json();
+		console.log(cards)
+
+	
+	if (cards.length == 0) document.getElementById("card-list-empty").value = "no cards :(";
+	
+	else{
+		document.getElementById("card-list-empty").value = "";
+		for (let i of cards) {
+			container.innerHTML += rendercard(i);
+		}
+	}
+	
+	
+	
+}
+
+function rendercard(card) {
+	let html = "";
+	
+	html += "<div class=\"card-list-item\">";
+	html += "<h1>" + card.product_name + "</h1>";
+		html += "<p>" + card.profile_name + "</p>";
+
+	html += "<p>" + card.strength + "</p>";
+	
+	html += "</div>";
+	
+	return html;
+}
+
+
+	
+	
+
+tabswitcher("upload")
 
 
 document.getElementById("uploader-button").addEventListener("click", uploadhandler);
 document.getElementById("ocr-button").addEventListener("click", ocrhandler)
+
+document.getElementById("topbar-scan").addEventListener("click", () => {tabswitcher("upload")})
+document.getElementById("topbar-cards").addEventListener("click", () => {tabswitcher("cards")})
+
+
+document.getElementById("form-submit").addEventListener("click", savecard);
+document.getElementById("form-cancel").addEventListener("click", cancelform);
+
+
+
+
 
