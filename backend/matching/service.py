353cb7387ef8eeea8dfa90bcf472383193ec4bdb
ericxio <ericxio2010@gmail.com>
2026-08-04 22:09:28 -0500

    matching

---
+++ backend/matching/service.py
@@ -0,0 +1,56 @@
+from rapidfuzz import fuzz
+from backend.cards import service as cardservice
+
+
+threshold = 60
+
+def similarity(a,b):
+    if a is None or b is None or len(a) == 0 or len(b) == 0: return 0.0;
+
+    a = a.lower().rstrip().lstrip()
+    b = b.lower().rstrip().lstrip()
+
+    basicscore = fuzz.ratio(a, b)
+    partialscore = fuzz.partial_ratio(a, b)
+    tokenscore = fuzz.token_sort_ratio(a, b)
+
+    return max(basicscore, partialscore, tokenscore)
+
+
+
+def findmatch(text):
+    cards = cardservice.getallcards(
+    )
+
+    message = {
+            "matched": False,
+            "best_score": 0.0,
+            "best_product": None,
+            "message": "no cards saved"
+        }
+
+    if len(cards) == 0: return message
+
+    for i in cards:
+        score = similarity(text, i.ocr_text)
+        if score > message["best_score"]:
+            #message["matched"] = True
+            message["best_score"] = score
+            message["best_product"] = i.product_name
+            #message["message"] = "card found"
+
+    if message["best_score"] > threshold:
+        message['matched'] = True
+        message["message"] = "card found"
+
+    return message
+
+#     return
+#
+# a="the fintesgram pacert est is a mlti stage aerobiec capacit test that progresively gets more difficutl as ti continues     "
+# b="The FitnessGram Pacer test is a multistage aerobic capacity test that progressively gets more difficult as it continues. "
+#
+# print(similarity(a,b))
+#
+
+
